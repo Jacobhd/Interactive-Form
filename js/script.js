08410000e30ed5d29df55d43b722a7600c1c9beb
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-11-30 12:53:09 -0700

    Update script.js
    Cleaned up unnecessary code

--- js/script.js
+++ js/script.js
@@ -5,12 +5,13 @@
 
 // Global Variables
 const otherTitle = document.getElementById("other-title");
-const design = document.getElementById("design"); // select design id
-const colors = document.getElementById("color"); //select color id
-let shirtColors = document.getElementById("shirt-colors");  // select div shirt-colors id
+const design = document.querySelector("#design"); // select design id
+const colorDiv = document.getElementById("shirt-colors");  // select div color id
+const colorMenu = document.querySelector('#color');
+
 // console.log(design);
-// console.log(colors);
-// console.log(shirtColors);
+// console.log(colorDiv);
+// console.log(colorMenu);
 
 // On page load sets focus on "Name" input
 window.onload = () => {
@@ -28,87 +29,57 @@ title.addEventListener("change", () => {
   }
 })
 
-//T-Shirt Info Section
-// hide colors with message when select theme is selected and until design is changed
-// otherwise when option 1 is selected first 3 index shirtColors
-// otherwise when option 2 is selected last 3 shirtcolors
 
+//T-Shirt Info Section
 const fieldcheck = () => {
+  colorDiv.hidden = true;
 
   function shirtText() {
-  const option = shirtColors.textContent = "Please select a T-shirt theme"; //alters html
+  const option = colorDiv.textContent = "Please select Design"; //alters html
   return;
-}
+  }
+  //shirtText();
 
-// Selecting <option value from design id
-  design.options[0];
-  design.options[1];
-  design.options[2];
-  //console.log(design.options[0]);
-  //console.log(design.options[1]);
-  //console.log(design.options[2]);
-
-// Select <option value from color id
-//jsPunOptions
-  const cornFlowerBlue = colors.options[0];
-  const darkSlateGrey = colors.options[1];
-  const gold = colors.options[2];
-//console.log(gold);
-
-//heartJsOptions
-  const tomato = colors.options[3];
-  const steelBlue = colors.options[4];
-  const dimGrey = colors.options[5];
-//console.log(tomato);
-
-//  write for loop
-
-shirtText();
+  // Seperating heart code from pun code
+  [...colorMenu.children].filter(color => {
+    const heartCode = String.fromCharCode(parseInt(9829));  //heart symbol
+    if (color.textContent.includes(heartCode)) {
+      console.log(color.textContent);
+    } else {
+      console.log(color.textContent);
+    }
+  });
 
   design.addEventListener("change", () => {
-    if (design.options[1]) {
-      jsPunOptions();
-    }
-    if (design.options[2]) {
-      heartJsOptions();
-    }
-    if (design.options[0]) {
-      shirtText();
+    if (design === design.options[0]) {
+      return shirtText();
+    } else {
+      colorDiv.hidden = false
+      return colorMenu;
     }
   })
 }
 fieldcheck();
+console.log(design.options[0]);
+
+
 
 /* Notes, thoughts, questions, tests
 
+// Selecting <option value from design id
+// design.options[0];
+// design.options[1];
+// design.options[2];
+
 console.log(otherTitle);
 console.log(design);
-console.log(shirtColors);
-console.log(colors);
-console.log(designSelection);
-console.log(shirtColorSelection);
+// console.log(colorDiv);
+// console.log(colorMenu);
+//console.log(color.textContent);
 
 console.log(option);
 console.log(selectTheme);
 
 console.log(jsPuns.value);
-console.log(heartJs.text);
-
-console.log(jsPunsOptions);
-console.log(heartJsOptions);
-
-console.log(cornFlowerBlue);
-console.log(darkSlateGrey);
-console.log(gold);
-console.log(tomato);
-console.log(steelBlue);
-console.log(dimGrey);
-
-//shirtColors.style.display = "none";
-// if (design == design.options[0]) {
-//   shirtColors.style.display = "block";
-//   shirtColors.textContent = "Please select a T-shirt theme";
-// } else {
-//   colors;
-// }
+console.log(design.options[0]);
 */
