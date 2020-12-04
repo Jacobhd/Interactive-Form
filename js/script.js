439d77c14d6436b0dd7fa21683f1fe7363accafd
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-12-04 14:09:06 -0700

    Update script.js
    Successfully filtering heart options and hiding elements
    Questions on add / remove selected attribute

--- js/script.js
+++ js/script.js
@@ -2,15 +2,8 @@
 
 'use strict'
 
-
 // Global Variables
 const otherTitle = document.getElementById("other-title");
-let design = document.querySelector("#design"); // select design id
-let colorDiv = document.getElementById("shirt-colors");  // select div color id
-let colorMenu = document.querySelector("#color"); // select color id
-console.log(design);
-console.log(colorDiv);
-console.log(colorMenu);
 
 // On page load sets focus on "Name" input
 window.onload = () => {
@@ -30,52 +23,34 @@ title.addEventListener("change", () => {
 
 
 //T-Shirt Info Section
-const fieldcheck = () => {
-
-  colorDiv.hidden = true;
-
-  function hide() {
-    if (design.options[0]) {
-      let option = colorDiv.textContent = "Please select Design"; //alters html
-    }
-  }
-  //hide();
+const designCheck = () => {
+  const design = document.querySelector("#design");
+  const colorMenu = document.querySelector("#color");
+  const colorOptions = document.querySelectorAll('#color option');
+  colorMenu.hidden = true;
 
   design.addEventListener("change", (e) => {
-    colorDiv.hidden = false;
     let option = e.target.value;
-    if (option === design.options[2]) {
+    colorMenu.hidden = false;
+    for (let i = 0; i < colorOptions.length; i++) {
+      colorOptions[i].hidden = true;
+      colorOptions[i].removeAttribute("class", "select");
+    }
+
+    if (option === 'heart js') {
       [...colorMenu.children].filter(color => {
         const heartCode = String.fromCharCode(parseInt(9829));
         if (color.textContent.includes(heartCode)) {
-          return color.textContent;
+          color.hidden = false;
+          //colorMenu.addAttribute("selected", "colorOptions[3]");
         }
       });
-    } else if (option === design.options[1]) {
-      return color.textContent;
-    } else {
-      hide();
     }
+    // } else if (option === 'js puns') {
+    //
+    // } else {
+    //   colorMenu.value = "Please select Design";
+    // }
   });
 }
-fieldcheck();
-
-/* Notes, thoughts, questions, tests
-
-selecting <option value from design id
-design.options[0];
-design.options[1];
-design.options[2];
-
-console.log(otherTitle);
-console.log(design);
-console.log(colorDiv);
-console.log(colorMenu);
-console.log(color.textContent);
-
-console.log(option);
-console.log(selectTheme);
-
-console.log(jsPuns.value);
-console.log(design.options[0]);
-*/
+designCheck();
