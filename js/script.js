250b66e9606abc2d1105bf832f757fdecde973e3
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2020-12-09 13:49:15 -0700

    Update script.js
    Activity Section added
    Displays total cost (live)
    Working on -
    Day and Time data conflicts

--- js/script.js
+++ js/script.js
@@ -2,15 +2,31 @@
 
 'use strict'
 
-// Global Variables
+// * Global Variables *
 const otherTitle = document.getElementById("other-title");
 
-//T-Shirt Info Section
+// T-Shirt Info Section
 const design = document.querySelector("#design");
 const colorMenu = document.querySelector("#color");
 const colorOptions = document.querySelectorAll("#color option");
 
-// On page load sets focus on "Name" input
+// Activities Section
+const checkBox = document.querySelectorAll('input[type="checkbox"]');
+const activities = document.getElementsByClassName("activities");
+const activity = document.querySelector(".activities");
+let totalCost = document.createElement("totalCost");
+activity.append(totalCost);
+let activityCost = 0;
+// console.log(checkBox);
+// console.log(activities);
+// console.log(activity);
+// console.log(totalCost);
+
+
+// Payment Section
+// Validation Section
+
+// On page load, sets focus on "Name" input
 window.onload = () => {
  document.getElementById("name").focus();
 }
@@ -26,9 +42,9 @@ title.addEventListener("change", () => {
   }
 })
 
-
-//T-Shirt Info Section
-const designCheck = () => {
+//********************************************************************************
+//T-Shirt Info Section - Displays color options based on 'design' selection
+//const designCheck = () => {
   //const selectTheme = new Option('Please Select Design', 'selectdesign');
   colorMenu.hidden = true;
   design.addEventListener("change", (e) => {
@@ -59,5 +75,111 @@ const designCheck = () => {
           colorMenu[2].style.display = "block"
     }
   });
+//}
+//designCheck();
+
+//********************************************************************************
+// Activities Section - Allows selections based on avalibility and displays cost
+
+activity.addEventListener('change', (e) => {
+  let event = e.target;
+  let dataCost = event.getAttribute('data-cost');
+  let parsed = parseInt(dataCost);
+  if (event.checked) {
+    activityCost += parsed;
+    console.log(activityCost);
+  } else {
+    activityCost -= parsed;
+    console.log(activityCost);
+  }
+totalCost.textContent = `Total: $${activityCost}`;
+
+let dataDayAndTime = event.getAttribute('data-day-and-time');
+for (let i = 0; i < checkBox.length; i++) {
+
+  console.log();
+
 }
-designCheck();
+
+
+});
+
+
+// const activityCheck = () => {
+//   const mainCheckbox = checkBox[0];
+//   const jsFrameworks = checkBox[1];
+//   const jsLibs = checkBox[2];
+//   const express = checkBox[3];
+//   const jsNodes = checkBox[4];
+//   const buildTools = checkBox[5];
+//   const npm = checkBox[6];
+
+// Current problem being unchecking boxes doesn't always uncheck
+// [1] & [3] conflict, [2] & [4] conflict. If [0] is selected, all are disabled
+//display total of checkboxes as they're clicked ( variable select = data-cost )
+// jsFrameworks.addEventListener('change', (e) => {
+//   let event = e.target;
+//   express.disabled = event.checked
+//     //display totalCost
+// });
+//
+// jsLibs.addEventListener('change', (e) => {
+//   let event = e.target;
+//   jsNodes.disabled = event.checked
+//     //display totalCost
+// });
+//
+// express.addEventListener('change', (e) => {
+//   let event = e.target;
+//   jsFrameworks.disabled = event.checked
+//     //display totalCost
+// });
+//
+// jsNodes.addEventListener('change', (e) => {
+//   let event = e.target;
+//   jsLibs.disabled = event.checked
+//     //display totalCost
+// });
+
+
+// for (let i = 0; i < checkBox.length; i++) {
+//   checkBox[i].addEventListener('change', (e) => {
+//   if (checkBox[0].checked) {
+//     checkBox[4].disabled = true;
+//   }
+//   });
+// }
+
+// mainCheckbox.addEventListener('change', (e) => {
+//   let event = e.target;
+//   if (mainCheckbox == event.checked) {
+//     for (let i = 1; i < checkBox.length; i++){
+//       checkBox[i].disabled = event.checked;
+//     }
+//   }
+//     //display totalCost
+// });
+
+
+//   checkBox.addEventListener('change', (e) => {
+//     let event = e.target;
+//     for (let i = 0; i < checkBox.length; i++) {
+//       if ()
+//     }
+//
+//   });
+
+// }
+// activityCheck();
+
+//********************************************************************************
+
+
+/*
+Pseudocode
+
+// Current problem being unchecking boxes doesn't always uncheck
+// [1] & [3] conflict, [2] & [4] conflict. If [0] is selected, all are disabled
+//display total of checkboxes as they're clicked ( variable select = data-cost )
+
+*/
